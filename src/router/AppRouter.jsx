0bb739885743724f6dcc48be74f782e6502407b5
Omar Obregon <oobregon544@gmail.com>
2024-08-06 22:44:37 -0500

    mern calendar frontend

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,19 +1,34 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { LoginPage } from '../auth/pages/LoginPage';
 import { CalendarPage } from '../calendar/pages/CalendarPage';
+import { useAuthStore } from '../hooks/useAuthStore';
+import { useEffect } from 'react';
 
 export const AppRouter = () => {
-    const authStatus = 'authenticated';
+    // const authStatus = 'no-authenticated';
+    const { status, checkAuthToken } = useAuthStore();
+
+    useEffect(() => {
+        checkAuthToken();
+    }, []);
+
+    if (status === 'checking') {
+        return <h3>Cargando...</h3>;
+    }
 
     return (
         <Routes>
-            {authStatus === 'no-authenticated' ? (
-                <Route path='/auth/*' element={<LoginPage />} />
+            {status === 'not-authenticated' ? (
+                <>
+                    <Route path='/auth/*' element={<LoginPage />} />
+                    <Route path='/*' element={<Navigate to='/auth/login' />} />
+                </>
             ) : (
-                <Route path='/*' element={<CalendarPage />} />
+                <>
+                    <Route path='/' element={<CalendarPage />} />
+                    <Route path='/*' element={<Navigate to='/' />} />
+                </>
             )}
-
-            <Route path='/*' element={<Navigate to='/auth/login' />} />
         </Routes>
     );
 };
